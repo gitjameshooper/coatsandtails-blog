45915351c00802041b9760551471ffb06d629094
Jim Hooper <jim@massrelevance.com>
2015-03-07 11:55:11 -0600

    Add Terms Popup

--- wp-content/themes/aldehyde/js/custom-script.js
+++ wp-content/themes/aldehyde/js/custom-script.js
@@ -11,7 +11,7 @@ jQuery( document ).ready( function( $ ) {
 	$('.sub-menu > li > a').each(function(){
            var title = $(this).attr('title');
 
-           if(title !== undefined && title !== 'contact' && title !== 'submenu-title'){
+           if(title !== undefined && title !== 'contact'  && title !== 'submenu-title' && title !== 'terms' ){
            	var extJpg =  title.indexOf('.jpg') > -1;
            	var extPng =  title.indexOf('.png') > -1;
            	if(extPng || extJpg)
@@ -36,6 +36,32 @@ jQuery( document ).ready( function( $ ) {
 	}, function(){
 		$(this).parent().parent().find('.cover-banner img').hide();
    			$(this).parent().parent().find('.cover-banner img').attr('src', '');
+	});
+	// terms modal
+	$('a[title="terms"]').click(function(){
+		var dir = $(this).data('dir');
+		if($('#overlay_modal').text().length > 0){
+			$('#overlay_modal').remove();
+		}
+		$.ajax({
+			url:  "http://www.coatandtails.com/services/get_t_and_c.php",
+			success:function(h){
+				 
+				var payload = '<div class="modal fade" id="overlay_modal" tabindex="-1" role="dialog" aria-hidden="true">'
+						+ '<div class="modal-dialog modal-sm">'
+							+ '<div class="modal-content">'
+								+ '<div class="modal-header">'
+									+ '<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'
+									+ '<h4 class="modal-title t_c_title">Terms & Conditions</h4>'
+								+ '</div>'
+								+ '<div class="modal-body">' + h.data + '</div>'
+							+ '</div>'
+						+ '</div>'
+					+ '</div>';
+				$('body').append(payload);
+				$('#overlay_modal').modal('show');
+			}
+		});
 	});
 	// contact form
 	$('a[title="contact"]').click(function(e){
